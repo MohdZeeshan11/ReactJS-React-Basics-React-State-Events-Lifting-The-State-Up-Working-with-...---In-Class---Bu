8df2456606a75a14382c92d63b71b590b439794f
mohd zeeshan <mohdzeeshan7775@gamil.com>
2022-06-04 04:46:04 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,18 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state ={value:''};
 	};
 
+	
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button id='click' onClick={this.addText = ()=>{
+	        	this.setState({value:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"});
+	            }}>Click me !</button>
+				<p id='para'>{this.state.value}</p>
     		</div>
     	);
     }
